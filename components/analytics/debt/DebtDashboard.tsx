0cc15da2c903cfcf33831ea5877f49b5561fc1c0
NombrePrueba <mastelli@users.noreply.github.com>
2026-09-14 17:20:57 +0200

    Add manual stocks entry to Savings & Investment page with Accion/Bolsa/Precio/Cantidad fields

--- components/analytics/debt/DebtDashboard.tsx
+++ components/analytics/debt/DebtDashboard.tsx
@@ -9,6 +9,7 @@ import { DebtCharts } from "./DebtCharts"
 import { DebtCalendar } from "./DebtCalendar"
 import { Simulator } from "./Simulator"
 import { Recommendations } from "./Recommendations"
+import { ManualStocks } from "./ManualStocks"
 import {
   applyScenario,
   computeDerived,
@@ -87,6 +88,7 @@ export function DebtDashboard() {
       <AnalyticsHeader titleKey="Balance" showActions={false} />
       <Kpis base={base} scenario={scenario} flags={flags} />
       <Diagnosis flags={flags} />
+      <ManualStocks />
       <InputsSection items={items} onChange={setItems} />
       <DebtCharts d={scenario} items={items} projection={projection} months={months} onMonthsChange={setMonths} />
       <DebtCalendar items={items} />
